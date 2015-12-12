c8c178d236d0cb3165abb9714b5d2b62fe9940aa
Yrocky <983272765@qq.com>
2015-12-12 18:25:27 +0800

    Initial Commit

---
+++ HLLTasskyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLTasskyDemo
+//
+//  Created by Youngrocky on 15/12/12.
+//  Copyright © 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLTasskyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLTasskyDemo
+//
+//  Created by Youngrocky on 15/12/12.
+//  Copyright © 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
